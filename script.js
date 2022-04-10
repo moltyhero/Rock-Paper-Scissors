f20971782748a94571547d070730ebac3ca4e0dd
moltyhero <matanoded@gmail.com>
2022-04-10 04:02:51 +0300

    Create computerPlay and playRound

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Hello");
+
+
+
+function computerPlay() {
+    const options = ["rock", "paper", "scissors"];
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == computerSelection) return "It is a tie!"
+    else if (playerSelection == "rock")
+    {
+        if (computerSelection == "paper") return "You lose! Paper beats Rock";
+        else if (computerSelection == "scissors") return "You win! Rock beats Scissors";
+    }
+    else if (playerSelection == "paper") // Player selection is Paper
+    {
+        if (computerSelection == "scissors") return "You lose! Scissors beats Paper";
+        else if (computerSelection == "rock") return "You win! Paper beats Rock";
+    }
+    else if (playerSelection == "scissors") // Player selection is Scissors
+    {
+        if (computerSelection == "rock") return "You lose! Rock beats Scissors";
+        else if (computerSelection == "paper") return "You win! Scissors beats Paper";
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
